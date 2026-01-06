3f5c7e4bc2f3ca16da5bfdd9190157ed9348d9a9
Wix Remote Machine <support@wix.com>
2026-01-06 13:59:53 +0000

    initial-commit

---
+++ astro.config.mjs
@@ -0,0 +1,81 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import cloudProviderFetchAdapter from "@wix/cloud-provider-fetch-adapter";
+import wix from "@wix/astro";
+import monitoring from "@wix/monitoring-astro";
+import react from "@astrojs/react";
+import sourceAttrsPlugin from "@wix/babel-plugin-jsx-source-attrs";
+import dynamicDataPlugin from "@wix/babel-plugin-jsx-dynamic-data";
+import customErrorOverlayPlugin from "./vite-error-overlay-plugin.js";
+import wixPages from "@wix/astro-pages";
+import { PAGES_SUFFIX_PATH, parsePagesFromFile } from "@wix/vibe-routes-parser";
+import { join } from "node:path";
+
+const isBuild = process.env.NODE_ENV == "production";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [
+    {
+      name: "framewire",
+      hooks: {
+        "astro:config:setup": ({ injectScript, command }) => {
+          if (command === "dev") {
+            injectScript(
+              "page",
+              `const version = new URLSearchParams(location.search).get('framewire');
+              if (version){
+                const localUrl = 'http://localhost:3202/framewire/index.mjs';
+                const cdnUrl = \`https://static.parastorage.com/services/framewire/\${version}/index.mjs\`;
+                const url = version === 'local' ? localUrl : cdnUrl;
+                const framewireModule = await import(/* @vite-ignore */ url);
+                globalThis.framewire = framewireModule;
+                framewireModule.init({}, import.meta.hot);
+                console.log('Framewire initialized');
+              }`,
+            );
+          }
+        },
+      },
+    },
+    tailwind(),
+    wix({
+      htmlEmbeds: isBuild,
+      auth: true,
+    }),
+    isBuild ? monitoring() : undefined,
+    react({ babel: { plugins: [sourceAttrsPlugin, dynamicDataPlugin] } }),
+    {
+      name: "Watch router tsx file",
+      hooks: {
+        "astro:config:setup"({ addWatchFile }) {
+          addWatchFile(join(import.meta.dirname, PAGES_SUFFIX_PATH));
+        },
+      },
+    },
+    wixPages({
+      extendPages: async () => {
+        return parsePagesFromFile(import.meta.dirname);
+      },
+    }),
+  ],
+  vite: {
+    plugins: [customErrorOverlayPlugin()],
+  },
+  adapter: isBuild ? cloudProviderFetchAdapter({}) : undefined,
+  devToolbar: {
+    enabled: false,
+  },
+  image: {
+    domains: ["static.wixstatic.com"],
+  },
+  server: {
+    allowedHosts: true,
+    host: true,
+  },
+  security: {
+    checkOrigin: false
+  }
+});
